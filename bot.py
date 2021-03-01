348e94c43dd146d8a4ea2b1a1573c4f7d2a16e4e
Logan-Rose <logan.rose0329@gmail.com>
2021-02-28 23:44:52 -0500

    added flask page for front end

--- bot.py
+++ bot.py
@@ -1,2 +1,29 @@
-import websocket
-print("hello")
+import websocket, json, pprint
+
+SOCKET = "wss://stream.binance.com/ws/btcusdt@kline_1m"
+
+def onOpen(ws):
+    print("opened")
+
+def onClose(ws):
+    print("Closed")
+
+def onError(ws, error):
+    print(error)
+
+def onMessage(ws, message):
+    jsonMessage = json.loads(message)
+    #pprint.pprint(jsonMessage)
+
+    candle = jsonMessage['k']
+
+    isClosed = candle['x']
+    close = candle['c']
+
+    if isClosed:
+        print(close)
+
+
+
+ws = websocket.WebSocketApp(SOCKET,on_open=onOpen, on_close=onClose, on_error=onError, on_message=onMessage )
+ws.run_forever()
