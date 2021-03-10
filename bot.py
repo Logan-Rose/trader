43dbbc9142d3e383e9d58a02449d3117264e1a86
Logan-Rose <logan.rose0329@gmail.com>
2021-03-09 20:27:15 -0500

    added historical pricing data graph

--- bot.py
+++ bot.py
@@ -1,35 +1,39 @@
-import websocket, json, pprint, binance, requests, pandas, datetime
+import websocket, json, pprint, binance, requests, pandas, datetime, talib
 from matplotlib import pyplot as plt
+import plotly.graph_objects as go
+
 
 SOCKET = "wss://stream.binance.com/ws/"
 HTTP_ROOT = 'https://api.binance.com/api/v1/'
 SYMBOL = "BTCUSDT"
 STREAM = "@kline_"
 PERIOD = "1h"
+URL =  SOCKET + SYMBOL + STREAM + PERIOD
 
-url = SOCKET + SYMBOL + STREAM + PERIOD
+class bot:
+    def __init__(self):
+        self.name = "logan"
+    
+    def getHistoricalData(self, data, symbol, interval):
+            currentUrl= HTTP_ROOT + data + '?symbol=' + symbol + '&interval=' + interval
+            data = json.loads(requests.get(currentUrl).text)
 
-file1 = open("text.txt", "w")
+            df = pandas.DataFrame(data)
+            df.columns = ['open_time',
+                        'o', 'h', 'l', 'c', 'v',
+                        'close_time', 'qav', 'num_trades',
+                        'taker_base_vol', 'taker_quote_vol', 'ignore']
 
-def getHistoricalData(data, symbol, interval):
-        currentUrl= HTTP_ROOT + data + '?symbol=' + symbol + '&interval=' + interval
-        data = json.loads(requests.get(currentUrl).text)
+            df.index = [datetime.datetime.fromtimestamp(x/1000.0) for x in df.close_time]
+            return df
+    def plotData(self):
+        
+        values = self.getHistoricalData("klines", SYMBOL, PERIOD)
 
-        df = pandas.DataFrame(data)
-        df.columns = ['open_time',
-                    'o', 'h', 'l', 'c', 'v',
-                    'close_time', 'qav', 'num_trades',
-                    'taker_base_vol', 'taker_quote_vol', 'ignore']
+        prices = values['c'].astype('float')
 
-        df.index = [datetime.datetime.fromtimestamp(x/1000.0) for x in df.close_time]
-        return df
-
-values = getHistoricalData("klines", SYMBOL, PERIOD)
-
-prices = values['c'].astype('float')
-
-plt.plot(prices)
-plt.show()
+        plt.plot(prices)
+        plt.show()
 
 def onOpen(ws):
     print("opened")
@@ -48,9 +52,23 @@ def onMessage(ws, message):
 
     isClosed = candle['x']
     close = candle['c']
-
     if isClosed:
         print(close)
 
-#ws = websocket.WebSocketApp(url,on_open=onOpen, on_close=onClose, on_error=onError, on_message=onMessage )
-#ws.run_forever()
+
+logan = bot()
+function = getattr(talib, "RSI")
+frame = logan.getHistoricalData("klines", SYMBOL, PERIOD)
+result = function(frame['c'], 14)
+print(result[-1])
+
+
+fig = go.Figure(data=[go.Candlestick(x=frame['close_time'],
+                open=frame['o'],
+                high=frame['h'],
+                low=frame['l'],
+                close=frame['c'])])
+fig.show()
+
+# ws = websocket.WebSocketApp(URL,on_open=onOpen, on_close=onClose, on_error=onError, on_message=onMessage )
+# ws.run_forever()
--- frontend.py
+++ frontend.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
-
+from momentumIndicators import momentumIndicators
 app = Flask(__name__)
 
 @app.route("/")
 def helloWorld():
-    return render_template("home.html")
+    return render_template("home.html", momentumIndicators=momentumIndicators)
