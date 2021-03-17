e01ffd0a79874fcb76dd56eda5fa82af40491408
Logan-Rose <logan.rose0329@gmail.com>
2021-03-17 19:30:45 -0400

    UI Overhaul

--- frontend.py
+++ frontend.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from momentumIndicators import momentumIndicators
 from currenciesAvailable import currenciesAvailable
 from timePeriods import timePeriods
+#from bot import Bot
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['GET', 'POST'])
 def helloWorld():
+    print("++++")
+    print("Coin One:", request.form.get("coinOne"))
+    print("Coin Two:", request.form.get("coinTwo"))
+    print("Period:", request.form.get("period"))
+    vals = list(request.form.values())
+    for i in range(3,len(vals), 3):
+        print(vals[i] + ": buy: " + vals[i+1] + ", sell: " + vals[i+2])
+    print("++++")
     return render_template("home.html", momentumIndicators=momentumIndicators, currencies=currenciesAvailable, timePeriods=timePeriods)
-
-@app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
---
+++ static/app.js
@@ -0,0 +1,4 @@
+console.log("Helloo")
+particlesJS.load('particles-js', 'static/particles.json', function() {
+  console.log('callback - particles-js config loaded');
+});
