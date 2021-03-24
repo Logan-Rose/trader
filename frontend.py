5c9aa69ccb87c9e26692f7a8de3480760af9cf0e
Logan-Rose <logan.rose0329@gmail.com>
2021-03-24 19:42:06 -0400

    restructured flask templates

--- frontend.py
+++ frontend.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import json, pprint
 from bot import Bot
 
 app = Flask(__name__)
 
+
+
 @app.route("/", methods = ['GET', 'POST'])
+def home():
+    with open('./info.json') as f:
+        info = json.load(f)
+    indicators = info['momentumIndicators']
+    currencies = info['currenciesAvailable']
+    periods = info['timePeriods']
+    return render_template("home.html", indicators=indicators, currencies=currencies, periods=periods)
+
+
+@app.route("/run", methods = ['GET', 'POST'])
 def autoQuant():
 
     with open('./info.json') as f:
@@ -14,18 +26,18 @@ def autoQuant():
     currencies = info['currenciesAvailable']
     periods = info['timePeriods']
 
+    data = {}
     print("++++")
-    coinOne = request.form.get("coinOne")
-    print("Coin One:", coinOne)
-    coinTwo = request.form.get("coinTwo")
-    print("Coin Two:", coinTwo)
-    period = request.form.get("period")
-    print("Period:", period)
-    amount = int(request.form.get("amount"))
-    print("Amount:", amount)
+    data['coinOne'] = request.form.get("coinOne")
+    data['coinTwo'] = request.form.get("coinTwo")
+    data['period'] = request.form.get("period")
+    data['amount'] = request.form.get("amount")
     vals = list(request.form.values())[4:]
-
     for i in range(0, len(vals), 3):
-        print("ind:", vals[i], ",buy:", vals[i+1], ",sell:",vals[i+2])
-
-    return render_template("home.html", indicators=indicators, currencies=currencies, periods=periods)
+        currentInd = {}
+        currentInd['ind'] = vals[i]
+        currentInd['buy'] = vals[i+1]
+        currentInd['sell'] = vals[i+2]
+        data[str(i)] = currentInd
+    print()
+    return render_template("terminal.html", data=json.dumps(data), indicators=indicators, currencies=currencies, periods=periods)
